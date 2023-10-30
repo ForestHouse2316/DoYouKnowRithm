98b32e906167078e6defe1c285dabff386e12a58
ForestHouse <leo2316park@gmail.com>
2023-10-30 15:59:35 +0900

    Add file

--- BOJ/9466.cpp
+++ BOJ/9466.cpp
@@ -4,15 +4,15 @@ typedef pair<int, int> p;
 #define first first
 #define second second
 int T, N, in, currentStu, choose[100005];  // 1-indexed
+int visited[100005] = {0};  // 1-indexed : used to check circulation of the current DFS
 
 int main() {
-    ios_base::sync_with_stdio(false);
+    ios::sync_with_stdio(false);
     cin.tie(NULL);
     cin >> T;
 
     for (int t = 0; t < T; t++) {
-        int remained = 0;
-        bool checked[100005] = {false};  // 1-indexed : shows whether [n] is grouped/remained or not
+        int remained = 0, myIndex[100005] = {0};  // 1-indexed : shows whether [n] is grouped/remained or not
         cin >> N;
 
         for (int n = 1; n <= N; n++) {
@@ -21,33 +21,33 @@ int main() {
         }
 
         for (int n = 1; n <= N; n++) {  // DFS start
-            if (checked[n]) continue;
+            // if visited[i] == n, it means we visited that node in current loop
+            if (myIndex[n]) continue;
 
             stack<int> s;
-            bool visited[100005] = {false};  // 1-indexed : used to check circulation of the current DFS
-            checked[n] = true;
-            visited[n] = true;
+            visited[n] = n;
             s.push(n);
+            myIndex[n] = s.size();  // myIndex value starts with 1
 
             while (true) {
                 currentStu = s.top();
 
-                if (checked[choose[currentStu]] && !visited[choose[currentStu]]) {  // indicating grouped student
+                if (myIndex[choose[currentStu]] > 0 && visited[choose[currentStu]] != n) {  // previous loop visited => indicating grouped student
                     remained += s.size();
                     break;
                 }
 
-                if (!visited[choose[currentStu]]) {
-                    visited[choose[currentStu]] = true;  // IMPORTANT : Do this before push() if you don't use temp variable like currentStu
-                    checked[choose[currentStu]] = true;
-                    s.push(choose[currentStu]);  // new student
+                if (visited[choose[currentStu]] != n) {
+                    visited[choose[currentStu]] = n;  // IMPORTANT : Do this before push() if you don't use temp variable like currentStu
+                    s.push(choose[currentStu]);  // push newly chosen student
+                    myIndex[choose[currentStu]] = s.size();
                 }
                 else {  // if circulation found
                     int endpoint = choose[currentStu];
-                    while (s.top() != endpoint) s.pop();  // TODO 커스텀 스택 만들어서 endpoint가 어디에 위치하는지 쿼리한 후 remain을 빠르게 구하거나 자신의 인덱스를 저장하는 배열을 또 만들어 바로 구하도록 함
-                    s.pop();
-
-                    remained += s.size();
+//                    while (s.top() != endpoint) s.pop();  // TODO 커스텀 스택 만들어서 endpoint가 어디에 위치하는지 쿼리한 후 remain을 빠르게 구하거나 자신의 인덱스를 저장하는 배열을 또 만들어 바로 구하도록 함
+//                    s.pop();
+//                    remained += s.size();
+                    remained += myIndex[endpoint] - 1;
                     break;
                 }
             }

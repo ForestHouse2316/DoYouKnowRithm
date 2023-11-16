5d2c742b73474b91b903bfb87996149346d1194a
ForestHouse <leo2316park@gmail.com>
2023-11-16 17:24:03 +0900

    Add file

--- BOJ/1654.cpp
+++ BOJ/1654.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
-int K, N, sum, s, e, m, maxLen = 0, maxCutLen = 0, len[10002];
+int K, N, sum, maxLen = 0, maxCutLen = 0, len[10002];
+unsigned int s, m, e;
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -12,17 +13,13 @@ int main() {
     }
     s = 1;
     e = maxLen;
-    while (true) {
+    while (s <= e) {
         sum = 0;
         m = (s+e)/2;
         for (int k = 0; k < K; k++) {
             sum += len[k]/m;
         }
-        if (s >= e) {
-            maxCutLen = max(maxCutLen, m);
-            break;
-        }
-        else if (sum >= N) {
+        if (sum >= N) {
             maxCutLen = m;
             s = m+1;
         }

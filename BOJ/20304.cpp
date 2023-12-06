6ad2c7ec159a9c01b93283b60a9abd7496d99e18
ForestHouse <leo2316park@gmail.com>
2023-12-06 16:51:19 +0900

    Add file

--- BOJ/20304.cpp
+++ BOJ/20304.cpp
@@ -1,58 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
-int N, M, P[100000], amp, sum, shift, minSec, maxSec = -1, cSL, tPW;
-bool vis[1000001] = {false};
+int N, M, tPW, in, maxDist = 0, dist[1000005];
 queue<int> q;
-
-int XOR(int a, int b) {  // equal to security distance
-    shift = 20;
-    sum = 0;
-    amp = a ^ b;
-    while(shift-- > 0) {
-        sum += (amp>>shift)&1;
-    }
-    return sum;
-}
-
-int securityLevel(int pw) {
-    minSec = INT_MAX;
-    for (int m = 0; m < M; m++) {
-        minSec = min(minSec, XOR(pw, P[m]));
-    }
-    return minSec;
-}
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cin >> N >> M;
+    fill(dist, dist+N+5, -1);
+
     for (int m = 0; m < M; m++) {
-        cin >> P[m];
+        cin >> in;
+        q.push(in);
+        dist[in] = 0;
     }
 
-    for (int p = 0; p < 20; p++) {
-        tPW = 1 << p;
-        if (tPW > N) break;
-        q.push(tPW);
-        vis[tPW] = true;
-    }
-    while(!q.empty()) {  // start BFS about each bit of integer from 0 to 1
-        cSL = securityLevel(q.front());
-//        cout << "front | cSL > " << q.front() << " " << cSL << endl;
-        if (cSL > maxSec) {  // TODO = 인 경우라도 추후에 뒷쪽 비트가 바뀌면서 global minmum에 영향을 미치는 비트가 바뀔 수도 있음
-            maxSec = cSL;
-            for (int pos = 0; pos < 20; pos++) {
-                tPW = q.front() | (1<<pos);
-                if (tPW > N) break;
-                if (vis[tPW]) continue;  // if tPW == q.front(), it'll be passed by vis[tPW]
-                vis[tPW] = true;
-                q.push(tPW);
-            }
+    while(!q.empty()) {  // BFS
+        for (int shift = 0; shift <= 20; ++shift) {  // 2^19 < 1m < 2^20
+            if (q.front() & 1<<shift) tPW = q.front() & ~(1<<shift);
+            else tPW = q.front() | 1<<shift;
+
+            if (tPW > N) continue;
+            if (dist[tPW] >= 0) continue;
+
+            dist[tPW] = dist[q.front()]+1;
+            q.push(tPW);
         }
         q.pop();
     }
-    cSL = securityLevel(0);
-    maxSec = max(cSL, maxSec);
 
-    cout << maxSec;
+    for (int n = 0; n <= N; ++n) {
+        maxDist = max(maxDist, dist[n]);
+    }
+    cout << maxDist;
 }

6d8d4a598707b7b771fde9564339e7ef6f7c06b3
ForestHouse <leo2316park@gmail.com>
2023-12-13 17:21:22 +0900

    Add file

--- BOJ/4195.cpp
+++ BOJ/4195.cpp
@@ -1,44 +1,46 @@
 #include <bits/stdc++.h>
+#define MAX 100003
 using namespace std;
 
-int T, F;
+int T, F, idCnt, ra, rb, parent[MAX*2], num[MAX];
 string a, b;
+unordered_map<string, int> id;
+
+int root(int child) {
+    if (child == parent[child]) return child;
+    return root(parent[child]);
+}
+
+int assignId(string& name) {
+    if (id.find(name) != id.end()) return id[name];
+    id[name] = idCnt;
+    return idCnt++;
+}
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+    F = MAX-2;
     cin >> T;
     while (T--) {
-        map<string, string**> tree;  // name, group name pptr
-        list <string*> namePtr;  // group name pointer
-        map<string*, int> size;  // group name, size
-        list<string> names;
+        for (int i = 0; i < F*2+1; ++i) {
+            parent[i] = i;
+        }
+        fill(num, num+F+1, 1);
+        id.clear();
+        idCnt = 0;
+
         cin >> F;
-        while(F--) {
+        for(int f = 0; f < F; ++f) {
             cin >> a >> b;
-            if (tree.find(a) == tree.end()) {
-                if (tree.find(b) == tree.end()) {  // no a b
-                    names.push_front(b);
-                    namePtr.push_front(&(*names.begin()));
-                    tree[b] = &(*namePtr.begin());
-                    size[*tree[b]] = 1;
-                }
-                // no a
-                tree[a] = tree[b];  // a -> b
-                size[*tree[b]] += 1;
+            assignId(a); assignId(b);
+            ra = root(id[a]);
+            rb = root(id[b]);
+            if (ra != rb) {
+                num[rb] += num[ra];
+                parent[ra] = rb;
             }
-            else {
-                if (tree.find(b) == tree.end()) {  // no b
-                    tree[b] = tree[a];  // b -> a
-                    size[*tree[a]] += 1;
-                }
-                else if (**tree[a] != **tree[b]){  // both exist && different group
-                    size[*tree[b]] += size[*tree[a]];  // a group size
-                    size.erase(*tree[a]);
-                    *tree[a] = *tree[b]; // a group name ptr -> b group name ptr
-                }
-            }
-            cout << size[*tree[b]] << '\n';
+            cout << num[rb] << '\n';
         }
     }
 }

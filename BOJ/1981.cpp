266af3523707c94b8f7072c38c3cfa237ed80318
ForestHouse <leo2316park@gmail.com>
2023-12-14 17:18:24 +0900

    Add file

--- BOJ/1981.cpp
+++ BOJ/1981.cpp
@@ -5,13 +5,16 @@ using namespace std;
 #define h(a) get<2>(a)
 #define l(a) get<3>(a)
 #define f q.front()
+#define lf lq.front()
+#define fi first
+#define se second
 typedef tuple<int, int, int, int> ti;
-int N, s, e, m, tx, ty, th, tl, board[102][102], mx = 0, minGap, cycle;
+int N, s, e, m, tx, ty, th, tl, board[102][102], mi = INT_MAX, mx = 0, minGap, cycle;
 int dx[] = {0, 1, 0, -1}, dy[] = {1, 0, -1, 0};
-int vis[102][102];
+bool vis[102][102];
 
 // 최대값과 최소값을 고정해보자! 그것도 bin search로
-bool bfs(int limit) {
+bool bfs(int top, int bottom) {
     for (int i = 0; i < 102; ++i) {
         fill(vis[i], vis[i] + 102, INT_MAX);
     }
@@ -24,12 +27,14 @@ bool bfs(int limit) {
             tx = x(f) + dx[i];
             ty = y(f) + dy[i];
             if (tx < 0 || ty < 0 || tx >= N || ty >= N) continue;
-            th = max(h(f), board[ty][tx]);
-            tl = min(l(f), board[ty][tx]);
-            if (vis[ty][tx] <= th - tl) continue;
-            vis[ty][tx] = th-tl;
+            if (vis[ty][tx]) continue;
+//            th = max(h(f), board[ty][tx]);
+//            tl = min(l(f), board[ty][tx]);
+//            if (vis[ty][tx] <= th - tl) continue;
+//            vis[ty][tx] = th-tl;
+//            if (th-tl > limit) continue;
+            vis[ty][tx] = true;
 //            cout << "On " << tx << "-" << ty << " : diff = " << th-tl << endl;
-            if (th-tl > limit) continue;
             if (tx == N-1 && ty == N-1) return true;
             q.emplace(tx, ty, th, tl);
         }
@@ -46,18 +51,33 @@ int main() {
         for (int col = 0; col < N; ++col) {
             cin >> board[row][col];
             mx = max(mx, board[row][col]);
+            mi = min(mi, board[row][col]);
         }
     }
-    s = abs(board[0][0] - board[N-1][N-1]);
-    e = mx;
-    while (s <= e) {
-        m = (s+e)/2;
-        cout << "trying : " << m << endl;
-        if (bfs(m)) {
-            minGap = m;
-            e = m-1;
-        }
-        else s = m+1;
-    }
+//    s = abs(board[0][0] - board[N-1][N-1]);
+//    e = mx;
+//    while (s <= e) {
+//        m = (s+e)/2;
+//        cout << "trying : " << m << endl;
+//        if (bfs(m)) {
+//            minGap = m;
+//            e = m-1;
+//        }
+//        else s = m+1;
+//    }
+
+
+
     cout << minGap;
 }
+
+
+/*
+5
+5 6 6 6 9
+4 9 9 6 9
+4 4 4 6 9
+9 9 4 5 9
+9 9 9 7 5
+
+*/
